e68c0d1a3c05ba491ed0e42176de3145cdbecd21
cforcomputer <patjobs@pm.me>
2020-03-28 17:13:20 -0700

    Trying to get selenium to work by modifying the settings of umatrix before going through and copying pages from chegg to make for faster load times and possible compatibility with google chrome.

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import base64
 import csv
 import os.path
+import os
 import pickle
 import random
 import re
@@ -16,6 +17,11 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
+
+ua = UserAgent()
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # get project directory
+global oper_os  # os selector
 
 
 # Run program
@@ -32,7 +38,7 @@ def save_counter(number_to_save):
 # This method encodes title and description into base64 to preserve line breaks
 # and special characters.
 # It also solves the problem of csv writer defaulting ',' as "new row" delimiter
-def write_row(number, title, description, category, q_number):
+def write_row(number, title, description, category):
     print(number + title + description + category)
     encoded_title = base64.b64encode(title.encode())
     encoded_description = base64.b64encode(description.encode())
@@ -46,9 +52,11 @@ my_fields = ['number', 'title', 'description', 'category']  # csv columns  # for
 
 
 # Set the string value for the operating system to be used
-def set_os(os_input):
+def set_os(os_input, option):
     global new_os_input
+    global oper_os
     new_os_input = os_input
+    oper_os = option
     print("New OS set to: " + new_os_input)
 
 
@@ -73,11 +81,27 @@ def set_options():
     # Perform all functions required to gather the html of a page while avoiding bot detection
     # Must be outside 'collect_page()' so that a new instance is not created for each iteration
     opts = Options()
-    opts.add_argument("Mozilla/5.0 (compatible; GoogleDocs; apps-spreadsheets; +http://docs.google.com)")
+    # opts.add_argument("user-agent=Mozilla/5.0 (compatible; GoogleDocs; apps-spreadsheets; +http://docs.google.com)")
+    # opts.add_argument("user-agent=" + ua.chrome)
     opts.add_argument("--window-size=320,200")
     opts.add_extension("umatrix/extension_1_4_0_0.crx")
     browser = webdriver.Chrome(options=opts, executable_path=new_os_input)
     chrome_browser = browser
+    # Set uMatrix session settings
+    # Navigate to uMatrix session settings page
+    chrome_browser.get("chrome-extension://ogfcmafjalglgifnmanfmnieipoejdcf/dashboard.html#user-rules")
+    # switch to iframe to be able to access the user settings
+    frame = chrome_browser.find_element_by_xpath('/html/body/iframe')
+    chrome_browser.switch_to.frame(frame)
+    # upload user settings
+    importer = chrome_browser.find_element_by_id("importButton")
+    importer.clear()
+    if oper_os == 1:  # windows
+        importer.send_keys(ROOT_DIR + '\\umatrix\\my-umatrix-rules.txt')
+    elif oper_os == 2:  # if linux
+        importer.send_keys(ROOT_DIR + '/umatrix/my-umatrix-rules.txt')
+
+    chrome_browser.find_element_by_id("commitButton").click()  # commit changes
 
 
 # This method uses selenium to extract the html from the page and preserve the session cookies
@@ -95,7 +119,7 @@ def collect_page(q_number):
         url = 'https://www.chegg.com/homework-help/questions-and-answers/q' + str(q_number)  # Define URL
         chrome_browser.get(url)  # the url
         print(str(x) + " - Scraping: " + url)
-        time.sleep(random.uniform(0.5, 1))
+        # time.sleep(random.uniform(0.5, 1))
 
         content = chrome_browser.page_source  # grab html from the page
         return content  # method then returns the html content of the page
@@ -104,79 +128,102 @@ def collect_page(q_number):
 
 
 def start_program():
-    question_list = check_for_numberlist()
-    # Should reset at the start of each loop
-    title_string = ''
-    desc_string = ''
-    temp_string_array = []  # holds the final output values written to questions.csv
-    # current_question = read_increment_to_use()  # find a random question to scrape (no duplicates)
-    q_number = question_list[x]  # assign list question_list the increment [1++] to be question number
+    q_number = 0
+    x = 0
 
-    # page = urlopen(req).read()  # take request and read
-    soup = BeautifulSoup(collect_page(q_number), 'html.parser')  # parse request to html
-
-    # save counter each loop in event of error/crash
-    save_counter(x)
-
-    # Find the question title
-    if soup.select('h1[class*="PageHeading-"]'):  # if the page is a q&a page
-
-        for EachTitle in soup.select('h1[class*="PageHeading-"]'):
-            # Concat. each separate string to fill one row
-            title_string += EachTitle.get_text(strip=True)
-
-        # Find the full question text
-        for EachDescription in soup.select('section[class*="QuestionBody__QuestionBodyWrapper-sc-"]'):
-            # Concat. each separate string to fill one row
-            desc_string += EachDescription.get_text(strip=True)
-
-        # Prototyping finding the transcript script
-        # Appends the transcribed image text to the end of the description
-        if soup.select('div[class*="Transcript__TranscriptContent-sc-"]'):
-            desc_string += soup.find('div[class*="Transcript__TranscriptContent-sc-"]')
-
-        # Add a loop to gather the breadcrumb name of a page
-        # results = soup.find(lambda tag: " questions and answers" in tag.string if tag.string else False)
-        # Javascript parser for identifying specific breadcrumbs
-        # Param result = string containing breadcrumb script
-        scripts = soup.find_all("script")
-        category = ''
-        for script in scripts:
-            text = script.text
-            m_text = text.split(',')
-            for n in m_text:
-                if '"parentSubject":' in n:
-                    category = n  # Get subject name from script
-        match = re.search(r':"(.*?)"', category)  # Apply regex to grab only second word
-        category = match.group(1)  # remove quotes
-
-        print(q_number + title_string + desc_string + category)
-        # Fill the csv file with the gathered title, description, and category
-        write_row(q_number, title_string, desc_string, category)
-
-    else:  # Otherwise move to the next function
-        pass
-
-
-# Menu for selecting which driver to use
-def menu():
-    # global new_os_input
-    # Number that stores the current question number being scraped
-    # global q_number  # set to 0
-    # Incrementer for keeping track of position in list
-    global incrementer  # set to 0
-    # Counter value in start_program()
-    global x  # set to 0
-
-    # Set default operating system at start to windows
-    set_os('drivers/chromedriver_windows64/chromedriver.exe')
     try:
         incrementer = check_for_incrementer()  # return the current increment value if it exists
     except FileNotFoundError:
         print("Incrementer file does not exist")
         incrementer = 0
-    q_number = 0
-    x = 0
+
+    question_list = check_for_numberlist()
+    # Should reset at the start of each loop
+    title_string = ''
+    desc_string = ''
+
+    try:
+        # current_question = read_increment_to_use()  # find a random question to scrape (no duplicates)
+        for x in range(incrementer, 4000000):
+            q_number = question_list[x]  # assign list question_list the increment [1++] to be question number
+
+            # page = urlopen(req).read()  # take request and read
+            soup = BeautifulSoup(collect_page(q_number), 'html.parser')  # parse request to html
+
+            # save counter each loop in event of error/crash
+            save_counter(x)
+
+            # Find the question title
+            if soup.select('h1[class*="PageHeading-"]'):  # if the page is a q&a page
+
+                for EachTitle in soup.select('h1[class*="PageHeading-"]'):
+                    # Concat. each separate string to fill one row
+                    title_string += EachTitle.get_text(strip=True)
+
+                # Find the full question text
+                for EachDescription in soup.select('section[class*="QuestionBody__QuestionBodyWrapper-sc-"]'):
+                    # Concat. each separate string to fill one row
+                    desc_string += EachDescription.get_text(strip=True)
+
+            # Prototyping finding the transcript script
+            # Appends the transcribed image text to the end of the description
+            if soup.select('div[class*="Transcript__TranscriptContent-sc-"]'):
+                desc_string += soup.find('div[class*="Transcript__TranscriptContent-sc-"]')
+
+            # Add a loop to gather the breadcrumb name of a page
+            # results = soup.find(lambda tag: " questions and answers" in tag.string if tag.string else False)
+            # Javascript parser for identifying specific breadcrumbs
+            # Param result = string containing breadcrumb script
+            scripts = soup.find_all("script")
+            category = ''
+            for script in scripts:
+                text = script.text
+                m_text = text.split(',')
+                for n in m_text:
+                    if '"parentSubject":' in n:
+                        category = n  # Get subject name from script
+            match = re.search(r':"(.*?)"', category)  # Apply regex to grab only second word
+            category = match.group(1)  # remove quotes
+
+            print(q_number + title_string + desc_string + category)
+            # Fill the csv file with the gathered title, description, and category
+            write_row(q_number, title_string, desc_string, category)
+
+        else:  # Otherwise move to the next function
+            pass
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(e, exc_type, fname, exc_tb.tb_lineno)
+        # Export error log to file
+        m = open("export/error_log.txt", "a")
+        save_counter(x)
+        m.write(str(e))
+        m.write('\n')
+        m.write('\n')
+        m.write("@" + str(datetime.now()))
+        m.write('\n')
+        m.write("Iteration interrupted at question: " + str(q_number))
+        m.close()
+        # Wait between 8 and 9 minutes if blocked
+        time.sleep(random.randint(200, 400))
+        pass
+    except PermissionError as e:
+        print('Permission Error, now sleeping for 15 seconds')
+        print(e)
+        a = 15
+        while a != 0:
+            print(a)
+            time.sleep(1)
+            a = a - 1
+
+
+# Menu for selecting which driver to use
+def menu():
+    # Incrementer for keeping track of position in list
+    global incrementer  # set to 0
+    # Counter value in start_program()
+    global x  # set to 0
 
     print("************WELCOME TO CHEGGSCRAPER************")
     print()
@@ -186,49 +233,29 @@ def menu():
      M: Set Operating System
      Q: Quit
      """)
+    # Starting the program
     if choice == "S" or choice == "s":
-        # Run through every page in the database and store url to text file
-        for x in range(incrementer, 4000000):
-            try:
-                set_options()
-                start_program()
-                # Exception writes error to a file and saves the question on error
-                # for later reference
-            except Exception as e:
-                exc_type, exc_obj, exc_tb = sys.exc_info()
-                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                print(e, exc_type, fname, exc_tb.tb_lineno)
-                # Export error log to file
-                m = open("export/error_log.txt", "a")
-                save_counter(x)
-                m.write(str(e))
-                m.write('\n')
-                m.write('\n')
-                m.write("@" + str(datetime.now()))
-                m.write('\n')
-                m.write("Iteration interrupted at question: " + str(q_number))
-                m.close()
-                # Wait between 8 and 9 minutes if blocked
-                time.sleep(random.randint(200, 400))
-                pass
-            except PermissionError as e:
-                print('Permission Error, now sleeping for 15 seconds')
-                a = 15
-                while a != 0:
-                    print(a)
-                    time.sleep(1)
-                    a = a - 1
+        try:
+            set_options()
+            start_program()
+            # Exception writes error to a file and saves the question on error
+            # for later reference
+        except Exception as e:
+            print("Error starting program")
+            print(e)
 
+    # Manually setting the x-counter position
     elif choice == "B" or choice == "b":
         try:
             num_to_save = input("Enter the new increment value: ")
             save_counter(num_to_save)
-            print("Saved " + num_to_save + " to file")
+            print("Set " + num_to_save + " to current counter")
             menu()
         except ValueError as e:
             print("Please enter an integer 0-10000000")
+            print(e)
             menu()
-
+    # Selecting the operating system specific driver
     elif choice == "M" or choice == "m":
         choice = input("Please select operating environment\n"
                        "Type the number to select\n"
@@ -236,11 +263,12 @@ def menu():
                        "2. Linux\n")
 
         if choice == "1":
-            set_os("drivers/chromedriver_windows64/chromedriver.exe")
+            set_os("drivers/chromedriver_windows64/chromedriver.exe", 1)
         elif choice == "2":
-            set_os("drivers/chromedriver_linux64_81/chromedriver")
+            set_os("drivers/chromedriver_linux64_81/chromedriver", 2)
         else:
             menu()
+    # Quitting the program
     elif choice == "Q" or choice == "q":
         sys.exit()
     else:
@@ -249,18 +277,16 @@ def menu():
         menu()
 
 
-# should be called once when the program runs
-# Checks to see if numberlist.data exists, if it does, then set question_list to the
-# return value of read_next_question_list
+# Called once at the start of program to check if numberlist exists
+# If the numberlist does not exist, generate the numberlist
+# Otherwise, read from the existing numberlist
 def check_for_numberlist():
     # If the path doesn't exist, or file empty --> call generate a range of random numbers
-    #
     if not path.exists("export/numberlist.data") or os.stat("export/numberlist.data").st_size == 0:
         generate_num_range()
         print("***generated list***")
         menu()
     else:
-        print("Reading existing list")
         question_list = read_next_question_list()  # instance access for random number list
         return question_list
 
@@ -293,4 +319,6 @@ def generate_num_range():
 
 
 if __name__ == '__main__':
+    # Set default operating system at start to windows
+    set_os('drivers/chromedriver_windows64/chromedriver.exe', 1)
     main()
